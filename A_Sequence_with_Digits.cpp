6468eab4cd04f9713b36ed1807fb0c218a506bda
Munawer-Taj <taj4580@gmail.com>
2022-09-17 16:07:12 +0600

    Codeforce Problems

---
+++ A_Sequence_with_Digits.cpp
@@ -0,0 +1,77 @@
+/*
+Date:   17 September 2022
+Problem Link:   https://codeforces.com/problemset/problem/1355/A
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define REVSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    ll num, query;
+    cin >> num >> query;
+    ll temp = num;
+    for (ll i = 1; i < query; i++)
+    {
+        temp = num;
+        int max_digit = -1;
+        int min_digit = 10;
+        while (temp > 0)
+        {
+            int rem = temp % 10;
+            temp /= 10;
+            max_digit = max(max_digit, rem);
+            min_digit = min(min_digit, rem);
+        }
+        if (min_digit == 0)
+            break;
+        num += (max_digit * min_digit);
+    }
+    cout << num << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
