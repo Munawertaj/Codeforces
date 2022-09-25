3d73d6eba926f11cda9f0c35ffa82d28fe08cb0e
Munawer-Taj <taj4580@gmail.com>
2022-09-25 17:30:08 +0600

    Codeforce Problems

---
+++ F_Selection_of_Personnel.cpp
@@ -0,0 +1,69 @@
+/*
+Date:   24 September 2022
+Problem Link:   https://codeforces.com/contest/630/problem/F
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define REVSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+ll combination(ll num, ll x)
+{
+    ull ans = 1;
+    for (int i = 0; i < x; i++)
+    {
+        ans *= (num - i);
+        ans /= (i + 1);
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll num;
+    cin >> num;
+    ull res = combination(num, 5) + combination(num, 6) + combination(num, 7);
+    cout << res;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    return 0;
+}
