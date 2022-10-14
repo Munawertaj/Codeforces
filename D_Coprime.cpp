fac376569de99f905e2562f384b352c7e5b7ee09
Munawer-Taj <taj4580@gmail.com>
2022-10-14 09:36:13 +0600

    Codeforce Problems

---
+++ D_Coprime.cpp
@@ -0,0 +1,87 @@
+/*
+Date:   13 October 2022
+Problem Link:
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define REVSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int size;
+    cin >> size;
+    vll num;
+    map<int, int> index;
+    int temp;
+    vector<pair<int, int>> nums;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> temp;
+        index[temp] = i + 1;
+    }
+    for (auto x : index)
+    {
+        nums.pb({x.second, x.ff});
+    }
+    SORT(nums);
+    size = nums.size();
+    int ans = -1;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            if (__gcd(nums[i].ss, nums[j].ss) == 1)
+            {
+                ans = max(ans, nums[i].ff + nums[j].ff);
+            }
+        }
+    }
+
+    cout << ans << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
---
+++ E_Scuza.cpp
@@ -0,0 +1,82 @@
+/*
+Date:   13 October 2022
+Problem Link:
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define REVSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int step, ques;
+    cin >> step >> ques;
+    vll height(step), count, index;
+    ll sum = 0, maximum = -1;
+    for (int i = 0; i < step; i++)
+    {
+        cin >> height[i];
+        sum += height[i];
+        count.pb(sum);
+        maximum = max(height[i], maximum);
+        index.pb(maximum);
+    }
+    int num;
+    for (int i = 0; i < ques; i++)
+    {
+        cin >> num;
+        if (num < index[0])
+        {
+            cout << "0 ";
+            continue;
+        }
+        int temp = uppbound(index, num);
+        cout << count[temp - 1] << " ";
+    }
+    cout << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
