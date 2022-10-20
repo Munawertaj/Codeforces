a2fa3597a87ababd14eced00d62511476405ad7c
Munawer-Taj <taj4580@gmail.com>
2022-10-21 00:26:18 +0600

    Codeforce Problems

---
+++ A_Laptops.cpp
@@ -0,0 +1,74 @@
+/*
+Date:   21 October 2022
+Problem Link:   https://codeforces.com/problemset/problem/456/A
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int num;
+    cin >> num;
+    vector<pair<int, int>> laptop;
+    int x, y;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> x >> y;
+        laptop.pb({x, y});
+    }
+    SORT(laptop);
+    for (int i = 0; i < num - 1; i++)
+    {
+        // cout << laptop[i].ff << " " << laptop[i].ss << nl;
+        if (laptop[i].ss > laptop[i + 1].ss)
+        {
+            cout << "Happy Alex";
+            return;
+        }
+    }
+    cout << "Poor Alex";
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    return 0;
+}
