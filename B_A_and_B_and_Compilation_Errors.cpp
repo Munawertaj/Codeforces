33524dd9d56b175ceefe163d33096a23ac206cd8
Munawer-Taj <taj4580@gmail.com>
2022-10-25 00:38:23 +0600

    Codeforce Problems

---
+++ B_A_and_B_and_Compilation_Errors.cpp
@@ -0,0 +1,91 @@
+/*
+Date:   25 October 2022
+Problem Link:   https://codeforces.com/problemset/problem/519/B
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int size;
+    cin >> size;
+    vii num1(size), num2(size - 1), num3(size - 2);
+    for (int i = 0; i < size; i++)
+        cin >> num1[i];
+
+    for (int i = 0; i < size - 1; i++)
+        cin >> num2[i];
+
+    for (int i = 0; i < size - 2; i++)
+        cin >> num3[i];
+
+    SORT(num1);
+    SORT(num2);
+    SORT(num3);
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (num1[i] != num2[j])
+        {
+            cout << num1[i] << nl;
+            break;
+        }
+        else
+            j++;
+    }
+    j = 0;
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (num2[i] != num3[j])
+        {
+            cout << num2[i] << nl;
+            break;
+        }
+        else
+            j++;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    return 0;
+}
