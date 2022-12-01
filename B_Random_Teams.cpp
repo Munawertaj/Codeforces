6a381388da3b0c242fbb18ceb6beb0238b8d9eff
Munawer-Taj <taj4580@gmail.com>
2022-12-01 19:50:24 +0600

    Codeforces

---
+++ B_Random_Teams.cpp
@@ -0,0 +1,77 @@
+/*
+Date:   01 December 2022
+Problem Link:   https://codeforces.com/problemset/problem/478/B
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+ll nC2(ll val)
+{
+    ll res = (val * (val - 1)) / 2;
+    return res;
+}
+
+void solve()
+{
+    ll participant, teams;
+    cin >> participant >> teams;
+    ll max_pair, min_pair;
+
+    ll temp, temp2;
+    temp = participant - teams + 1;
+    max_pair = nC2(temp);
+
+    temp = ceil((participant * 1.0) / teams);
+    temp2 = participant / teams;
+
+    ll x = participant % teams;
+    ll y = teams - x;
+
+    min_pair = (nC2(temp) * x) + (nC2(temp2) * y);
+
+    cout << min_pair << " " << max_pair;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    return 0;
+}
