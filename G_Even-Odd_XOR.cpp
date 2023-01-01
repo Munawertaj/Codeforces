78c357b8bffa68f73d40b15f8c7e570405e94712
Munawer-Taj <taj4580@gmail.com>
2023-01-01 14:56:20 +0600

    Codeforces

--- G_Even-Odd_XOR.cpp
+++ G_Even-Odd_XOR.cpp
@@ -1,120 +1,88 @@
+/*
+Date:   01 January 2023
+Problem Link:   https://codeforces.com/contest/1722/problem/G
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
 
 #include <bits/stdc++.h>
-int const MAXN = 1e5 + 5;
-int cnt[MAXN];
-using namespace std;
 #define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
 
 void solve()
 {
+    ll size;
+    cin >> size;
+    if (size == 3)
+        cout << "2 1 3";
+    else
+    {
+        vll ans(size);
+        if (size & 1)
+        {
+            ans[size - 1] = 0;
+            size--;
+        }
+        ll x, y;
+        x = (2 << 25);
+        y = (2 << 26);
+        ll temp = 1;
+        for (int i = 0; i < size; i += 2)
+        {
+            ans[i] = temp;
+            ans[i + 1] = temp + x;
+            temp++;
+        }
+        if (size % 4)
+        {
+            ans[1] += (y - x);
+            ans[3] += y;
+        }
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+    }
+    cout << nl;
 }
-
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-        ll a[n];
-
-        ll cnt = 262144, cnt2 = 524288;
-        // while (cnt <= cnt2)
-        //     cnt *= 2;
-        std::vector<int> v1, v2;
-
-        if (n % 8 == 1)
-        {
-            v1.push_back(9);
-            v1.push_back(2);
-            v1.push_back(8);
-            v1.push_back(7);
-            v1.push_back(3);
-            v1.push_back(0);
-            v1.push_back(4);
-            v1.push_back(6);
-            v1.push_back(5);
-
-            n -= 9;
-        }
-        if (n % 8 == 2)
-        {
-            v1.push_back(0);
-            v1.push_back(8);
-            v1.push_back(1);
-            v1.push_back(9);
-            v1.push_back(2);
-            v1.push_back(10);
-            v1.push_back(3);
-            v1.push_back(11);
-            v1.push_back(14);
-            v1.push_back(12);
-
-            n -= 10;
-        }
-        if (n % 8 == 3)
-        {
-
-            v1.push_back(2);
-            v1.push_back(1);
-            v1.push_back(3);
-
-            n -= 3;
-        }
-        if (n % 8 == 4)
-        {
-            v1.push_back(2);
-            v1.push_back(1);
-            v1.push_back(3);
-            v1.push_back(0);
-            n -= 4;
-        }
-        if (n % 8 == 5)
-        {
-            v1.push_back(2);
-            v1.push_back(0);
-            v1.push_back(4);
-            v1.push_back(5);
-            v1.push_back(3);
-            n -= 5;
-        }
-        if (n % 8 == 6)
-        {
-            v1.push_back(4);
-            v1.push_back(1);
-            v1.push_back(2);
-            v1.push_back(12);
-            v1.push_back(3);
-            v1.push_back(8);
-            n -= 6;
-        }
-        if (n % 8 == 7)
-        {
-            v1.push_back(1);
-            v1.push_back(2);
-            v1.push_back(3);
-            v1.push_back(4);
-            v1.push_back(5);
-            v1.push_back(6);
-            v1.push_back(7);
-            n -= 7;
-        }
-        for (int i = 0; i < v1.size(); i++)
-            cout << v1[i] << " ";
-        cnt2 = cnt / 2;
-        ll x=1, y=1;
-        for (int i = 0; i < (n / 2); i++)
-        {
-            cnt--;
-            cnt2--;
-            x ^= cnt;
-            y ^= cnt2;
-            cout << cnt << " " << cnt2 << " ";
-        }
-        //cout << x << " " << y << endl;
-
-        cout << endl;
-    }
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
 }
