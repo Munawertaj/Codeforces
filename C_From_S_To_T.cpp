dceeb038079db860c0775faca6f6548e9181ac21
Munawer-Taj <taj4580@gmail.com>
2023-02-20 23:58:10 +0600

    1194/C

---
+++ C_From_S_To_T.cpp
@@ -0,0 +1,80 @@
+/*
+Date:   20 February 2023
+Problem Link:   https://codeforces.com/problemset/problem/1194/C
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    string st1, ans, st2;
+    cin >> st1 >> ans >> st2;
+    map<char, int> count;
+    for (int i = 0; i < st2.size(); i++)
+        count[st2[i]]++;
+
+    int j = 0;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        if (ans[i] == st1[j] && j < st1.size())
+            j++;
+        else
+            count[ans[i]]--;
+        if (count[ans[i]] < 0)
+        {
+            cout << "NO" << nl;
+            return;
+        }
+    }
+    if (j != st1.size())
+        cout << "NO" << nl;
+    else
+        cout << "YES" << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
