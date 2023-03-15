32a63584f9c958caa535ec1966a092ff7dcb16b4
Munawer-Taj <taj4580@gmail.com>
2023-03-15 23:17:41 +0600

    158/C

---
+++ C_Cd_and_pwd_commands.cpp
@@ -0,0 +1,100 @@
+/*
+Date:   15 March 2023
+Problem Link:   https://codeforces.com/problemset/problem/158/C
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int num;
+    cin >> num;
+    string command, path;
+
+    vss folders;
+    while (num--)
+    {
+        cin >> command;
+        if (command[0] == 'c')
+        {
+            cin >> path;
+            string st;
+
+            if (path[0] == '/' && folders.size())
+                folders.clear();
+
+            for (int i = 0; i < path.size(); i++)
+            {
+                if (path[i] == '/')
+                {
+                    if (st.size())
+                        folders.pb(st);
+                    st.clear();
+                }
+                else if (path[i] == '.')
+                {
+                    folders.pop_back();
+                    i++;
+                }
+                else
+                    st.pb(path[i]);
+            }
+            if (st.size())
+                folders.pb(st);
+        }
+        else
+        {
+            cout << "/";
+            for (int i = 0; i < folders.size(); i++)
+                cout << folders[i] << "/";
+            cout << nl;
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
