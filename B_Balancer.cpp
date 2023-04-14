2cf7d72c46112da0eaec28159aac59e8f5d76746
Munawer Taj <taj4580@gmail.com>
2023-04-15 03:40:10 +0600

    440/B

--- B_Balancer.cpp
+++ B_Balancer.cpp
@@ -52,13 +52,49 @@ void solve()
         sum += match[i];
     }
     ll average = sum / box;
-
-    ll ans = 0, extra;
-    for (int i = 0; i < box - 1; i++)
+    ll j = 0, dist, extra, req;
+    ll ans = 0;
+    for (int i = 0; i < box; i++)
     {
-        extra = average - match[i];
-        match[i + 1] -= extra;
-        ans += abs(extra);
+        if (match[i] >= average)
+        {
+            extra = match[i] - average;
+            match[i] = average;
+            ans += extra;
+            match[i + 1] += extra;
+            continue;
+        }
+        else
+        {
+            while (j < box)
+            {
+                if (match[j] <= average)
+                {
+                    j++;
+                    continue;
+                }
+                else
+                {
+                    req = average - match[i];
+                    extra = match[j] - average;
+                    dist = j - i;
+                    if (extra >= req)
+                    {
+                        ans += (dist * req);
+                        match[j] -= req;
+                        match[i] = average;
+                        break;
+                    }
+                    else
+                    {
+                        ans += (dist * extra);
+                        match[j] = average;
+                        match[i] += extra;
+                        j++;
+                    }
+                }
+            }
+        }
     }
     cout << ans;
 }
