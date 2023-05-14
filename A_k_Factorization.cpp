c598b5ee5fb4cd4e3d287f42dd2b7e39e9ae4353
Munawer Taj <taj4580@gmail.com>
2023-05-14 12:09:25 +0600

    797/A

---
+++ A_k_Factorization.cpp
@@ -0,0 +1,82 @@
+/*
+Date:   14 May 2023
+Problem Link:   https://codeforces.com/problemset/problem/797/A
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int num, products;
+    cin >> num >> products;
+    vii multiples;
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (products == 1)
+            break;
+        if (num % i == 0)
+        {
+            while (num % i == 0 && products > 1)
+            {
+                multiples.pb(i);
+                products--;
+                num /= i;
+            }
+        }
+    }
+    if (products > 1 || num < 2)
+        cout << "-1";
+    else
+    {
+        for (auto x : multiples)
+            cout << x << " ";
+        cout << num;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin>>tc;
+    while (tc--)
+        solve();
+    return 0;
+}
