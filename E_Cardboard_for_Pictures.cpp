94a99aab5d20fbcaa4fba8d768982e2ec623cf96
Munawer Taj <taj4580@gmail.com>
2023-07-22 14:56:34 +0600

    Round 886 contest-E

---
+++ E_Cardboard_for_Pictures.cpp
@@ -0,0 +1,95 @@
+/*
+Date:   22 July 2023
+Problem Link:
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define ms(a, b) memset(a, b, sizeof(a))
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+ll calculate(vii &paint, ll total, ll w)
+{
+    ll val;
+    for (int i = 0; i < paint.size(); i++)
+    {
+        val = (paint[i] + w + w);
+        total -= (val * val);
+        if (total < 0)
+            break;
+    }
+    return total;
+}
+
+void solve()
+{
+    ll size, board;
+    cin >> size >> board;
+    vii paint(size);
+    for (int i = 0; i < size; i++)
+        cin >> paint[i];
+
+    ll total;
+    ll low = 1, high = 1e9, mid;
+    while (high - low > 1)
+    {
+        mid = (high + low) / 2;
+        total = calculate(paint, board, mid);
+        if (total < 0)
+            high = mid - 1;
+        else if (total > 0)
+            low = mid + 1;
+        else
+        {
+            low = mid;
+            break;
+        }
+    }
+    while (calculate(paint, board, low) != 0)
+        low++;
+    cout << low << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
