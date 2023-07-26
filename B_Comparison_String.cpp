92e450f96d3f5869cfabb94f7ae54aaf7b2d3a11
Munawer Taj <taj4580@gmail.com>
2023-07-26 13:36:01 +0600

    1837/B

---
+++ B_Comparison_String.cpp
@@ -0,0 +1,70 @@
+/*
+Date:   26 June 2023
+Problem Link:   https://codeforces.com/problemset/problem/1837/B
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int size;
+    cin >> size;
+    string st;
+    cin >> st;
+    int count, ans = 2;
+    for (int i = 1; i < size; i++)
+    {
+        count = 1;
+        while (st[i] == st[i - 1])
+        {
+            i++;
+            count++;
+        }
+        ans = max(ans, count + 1);
+    }
+    cout << ans << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
