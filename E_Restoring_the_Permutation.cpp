ef59a608c77bfe026bd4cc2a1f75b96923c61e8e
Munawer Taj <taj4580@gmail.com>
2023-08-08 21:18:53 +0600

    1506/E

---
+++ E_Restoring_the_Permutation.cpp
@@ -0,0 +1,99 @@
+/*
+Date:   08 August 2023
+Problem Link:   https://codeforces.com/problemset/problem/1506/E
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int size;
+    cin >> size;
+    vii num(size), minimum(size), maximum(size);
+    set<int> check1, check2;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+        check1.insert(i+1);
+        check2.insert(i+1);
+    }
+
+    minimum[0] = maximum[0] = num[0];
+    check1.erase(num[0]);
+    check2.erase(num[0]);
+
+    int j = num[0] - 1, k = 1;
+    for (int i = 1; i < size; i++)
+    {
+        if (num[i] == num[i - 1])
+        {
+            auto it = check1.begin();
+            minimum[i] = *it;
+            check1.erase(*it);
+
+            it = --check2.lower_bound(num[i]);
+            maximum[i]=*it;
+            check2.erase(*it);
+        }
+        else
+        {
+            minimum[i] = maximum[i] = num[i];
+            check1.erase(num[i]);
+            check2.erase(num[i]);
+        }
+    }
+
+    for (int val : minimum)
+        cout << val << " ";
+
+    cout << nl;
+
+    for (int val : maximum)
+        cout << val << " ";
+
+    cout << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
