4226494269b83ffab4db55b32c54a4ea874f221f
Munawer Taj <taj4580@gmail.com>
2023-08-11 19:59:24 +0600

    1201/C

---
+++ C_Maximum_Median.cpp
@@ -0,0 +1,87 @@
+/*
+Date:   11 August 2023
+Problem Link:   https://codeforces.com/problemset/problem/1201/C
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+void solve()
+{
+    int size, operation;
+    cin >> size >> operation;
+    vii num(size);
+    for (int i = 0; i < size; i++)
+        cin >> num[i];
+    SORT(num);
+    ll index = size / 2, diff, j = index + 1, count;
+    while (operation)
+    {
+        while (j < size)
+        {
+            if (num[j] != num[index])
+                break;
+            j++;
+        }
+        if (j == size)
+            break;
+
+        count = j - index;
+        diff = num[j] - num[index];
+        if (count * diff <= operation)
+        {
+            num[index] = num[j];
+            operation -= (count * diff);
+        }
+        else
+        {
+            num[index] += (operation / count);
+            operation = 0;
+        }
+    }
+    num[index] += (operation / (index + 1));
+    cout << num[index] << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin>>tc;
+    while (tc--)
+        solve();
+    return 0;
+}
