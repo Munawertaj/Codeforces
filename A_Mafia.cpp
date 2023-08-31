f53f28cf286d076afbd1e114ea09ea80d002be9a
Munawer Taj <taj4580@gmail.com>
2023-08-31 23:04:16 +0600

    348/A

---
+++ A_Mafia.cpp
@@ -0,0 +1,87 @@
+/*
+Date:   31 August 2023
+Problem Link:   https://codeforces.com/problemset/problem/348/A
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+bool isValid(ll val, vii &num)
+{
+    ll sum = 0;
+    for (ll x : num)
+    {
+        sum += val - x;
+        if (sum >= val)
+            return true;
+    }
+    return false;
+}
+
+void solve()
+{
+    int size;
+    cin >> size;
+    vii num(size);
+    ll sum = 0;
+    int maximum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+        sum += num[i];
+        maximum = max(maximum, num[i]);
+    }
+    ll low = maximum, high = sum, mid;
+    while (low < high)
+    {
+        mid = low + (high - low) / 2;
+        if (isValid(mid, num))
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    cout << high;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin>>tc;
+    while (tc--)
+        solve();
+    return 0;
+}
