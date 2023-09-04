29a2f352bcb12ae6b6f359b2e94e400063949539
Munawer Taj <taj4580@gmail.com>
2023-09-04 22:14:30 +0600

    676/C

---
+++ C_Vasya_and_String.cpp
@@ -0,0 +1,81 @@
+/*
+Date:   04 September 2023
+Problem Link:   https://codeforces.com/problemset/problem/676/C
+Author: Tareq Munawer Taj
+CSE, Rajshahi University
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define vull vector<ull>
+#define vss vector<string>
+#define FOR(i, n, x) for (int i = 0; i < n; i = i + x)
+#define FORR(i, n, x) for (int i = n; i >= 0; i = i - x)
+#define SORT(v) sort(v.begin(), v.end())
+#define RSORT(v) sort(v.rbegin(), v.rend())
+#define REVERSE(v) reverse(v.rbegin(), v.rend())
+#define lowbound(name, val) lower_bound(name.begin(), name.end(), val) - name.begin()
+#define uppbound(name, val) upper_bound(name.begin(), name.end(), val) - name.begin()
+#define matrix(x) vector<vector<x>>
+#define setdigit(n) fixed << setprecision(n)
+#define MOD(a, b) (a % b + b) % b
+#define lcm(a, b) ((a) * ((b) / __gcd(a, b)))
+#define ff first
+#define ss second
+#define nl "\n"
+#define PI (acos(-1.0))
+#define mod 1000000007
+#define INF (ll)1e17
+#define N 100005
+using namespace std;
+
+int longestSequence(string &st, int changes, char ch)
+{
+    int count = 0, j = 0;
+    int len = st.size();
+    for (int i = 0; i < len; ++i)
+    {
+        if (st[i] == ch)
+            count++;
+
+        if (count >= changes + 1)
+        {
+            if (st[j] == ch)
+                count--;
+            j++;
+        }
+    }
+    return len - j;
+}
+
+void solve()
+{
+    int size, changes;
+    cin >> size >> changes;
+    string st;
+    cin >> st;
+
+    int ans1 = longestSequence(st, changes, 'a');
+    int ans2 = longestSequence(st, changes, 'b');
+    cout << max(ans1, ans2);
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+    return 0;
+}
